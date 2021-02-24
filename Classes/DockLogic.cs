671cc3853ce902386cd6e2cd971242ea1a2901a9
EpicMice <micecd@gmail.com>
2021-02-24 15:55:16 -0600

    Add project files.

---
+++ Classes/DockLogic.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestThing2.Classes
+{   
+
+    public enum DockContentType
+    {
+        DIV
+    }
+
+    public class DockContent
+    {
+        public string ID { get; set; }
+
+        public DockContentType DCT = DockContentType.DIV;
+
+        public bool Active = false;
+
+        public void SetActive(bool b)
+        {
+            this.Active = b;
+        }
+
+        public void OnTabDragStart()
+        {
+            Console.WriteLine("Dragging tab: " + ID);
+        }
+        public void OnTabDragEnd()
+        {
+            Console.WriteLine("Ending drag: " + ID);
+        }
+
+        //tab element reference?        
+        //content element reference?
+
+    }
+
+    public class DockInstance
+    {
+        public string Dock_ID;
+        public Dictionary<string, DockContent> Content_Map = new Dictionary<string, DockContent>();
+        
+        public void AddTabWithContent(DockContent dcontent)
+        {
+            Content_Map.Add(dcontent.ID, dcontent);
+            if(Content_Map.Count == 1)
+            {
+                dcontent.Active = true;
+            }
+        }
+
+        private void PerformSetActive(string id, bool make_active)
+        {
+            var item = Content_Map.Values.FirstOrDefault(e => e.Active);
+            if(item != null)
+            {
+                if(item.ID != id)
+                {
+                    item.SetActive(false);
+                    Content_Map[id].SetActive(true);
+                }
+            }
+        }
+
+        public void SetActive(string id, bool make_active)
+        {
+            this.PerformSetActive(id, make_active);
+        }
+
+        public void SetActive(DockContent dcontent, bool make_active)
+        {
+            this.PerformSetActive(dcontent.ID, make_active);
+        }
+
+        private void PerformRemoveTabAndContent(string id)
+        {
+            Content_Map.Remove(id);
+            if(Content_Map.Count == 1)
+            {
+                Content_Map.Values.FirstOrDefault().SetActive(true);
+            }
+        }
+
+        public void RemoveTabAndContent(string id)
+        {
+            this.PerformRemoveTabAndContent(id);
+        }
+
+        public void RemoveTabAndContent(DockContent dcontent)
+        {
+            this.PerformRemoveTabAndContent(dcontent.ID);
+        }
+
+    }
+
+    public class DockData
+    {
+
+        private int id = 0;
+        public string MakeID()
+        {
+            return "id_"+(id++);
+        }
+
+        public Dictionary<string, DockInstance> Dock_Map = new Dictionary<string, DockInstance>();
+
+        public Dictionary<string, DockContent> Dock_Tabs = new Dictionary<string, DockContent>();
+
+        public DockData()
+        {
+            //load config for docks
+
+            //sample config data
+
+            var dock1 = CreateDock();
+            var tab1 = CreateTabWithContent();
+            var tab2 = CreateTabWithContent();
+
+            dock1.AddTabWithContent(tab1);
+            dock1.AddTabWithContent(tab2);
+
+            dock1 = CreateDock();
+            tab1 = CreateTabWithContent();
+            tab2 = CreateTabWithContent();
+
+            dock1.AddTabWithContent(tab1);
+            dock1.AddTabWithContent(tab2);
+        }
+
+        public DockContent CreateTabWithContent()
+        {
+            DockContent dcontent = new DockContent();
+            dcontent.ID = MakeID();
+
+            return dcontent;
+        }
+
+        public DockInstance CreateDock()
+        {
+            DockInstance inst = new DockInstance();
+
+            inst.Dock_ID = MakeID();
+            this.Dock_Map.Add(inst.Dock_ID, inst);
+
+            return inst;
+        }
+    }
+}
---
+++ Classes/Utility.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestThing2.Classes{
+
+    public class StyleUpdate
+    {
+        public Dictionary<string, Action<(int,int)>> Style_Map = new Dictionary<string, Action<(int, int)>>();
+
+        public void AddStyleCallback(string id, Action<(int, int)> a)
+        {
+            this.Style_Map.Add(id, a);
+        }
+
+        public void UpdateStyle(string id, (int, int) ab)
+        {
+            Style_Map[id](ab);
+        }
+    }
+
+    public class EventActionGroup
+    {
+        private static int ID_Counter = 0;
+        public string ID = ""+EventActionGroup.ID_Counter++;
+
+        public Action OnMouseDown = () => { };
+        public Action OnMouseUp = () => { };
+        public Action OnDragStart = () => { };
+        public Action OnDragEnd = () => { };
+        public Action OnMouseMove = () => { };
+    }
+
+    public class InputService {
+
+        public Dictionary<string, EventActionGroup> RegisteredElements = new Dictionary<string, EventActionGroup>();
+
+        /*
+         
+        RegisteredElements is for storing EventActionGroups with an associated string (id)
+
+
+         
+        */
+
+        public int TrackDragX { get; set; } = 0;
+        public int TrackDragY { get; set; } = 0;
+
+        public string TrackString = null;
+
+        public string StartDrag(int x, int y)
+        {
+            this.TrackDragX = x;
+            this.TrackDragY = y;
+            return this.TrackString = Guid.NewGuid().ToString().Replace("-", "_");
+        }
+
+        public (bool, int, int) GetDragDifference(string id, int x, int y)
+        {
+            (bool, int, int) hold = (id == this.TrackString, x - TrackDragX, y - TrackDragY);
+            TrackDragX = 0;
+            TrackDragY = 0;
+            return hold;
+        }
+
+        public bool LMB_Down = false;
+        public bool RMB_Down = false;
+
+        public int ScreenX { get; set; } = 0;
+        public int ScreenY { get; set; } = 0;
+        public int ClientX { get; set; } = 0;
+        public int ClientY { get; set; } = 0;
+
+        public (int, int) CheckButtonState(MouseEventArgs e)
+        {
+            int LMB_State = 0, RMB_State = 0;
+
+            if (LMB_Down != (LMB_Down = (1 & e.Buttons) != 0))
+            {
+                if (LMB_Down)
+                {
+                    //button was just pressed
+                    LMB_State = 1;
+                }
+                if (!LMB_Down)
+                {
+                    //button was just released
+                    LMB_State = -1;
+                }
+            }
+
+            if (RMB_Down != (RMB_Down = (2 & e.Buttons) != 0))
+            {
+                if (RMB_Down)
+                {
+                    //button was just pressed
+                    RMB_State = 1;
+                }
+                if (!RMB_Down)
+                {
+                    //button was just released
+                    RMB_State = -1;
+                }
+            }
+
+            return (LMB_State, RMB_State);
+        }
+
+        public Dictionary<string, List<string>> MouseDownEventOrder = new Dictionary<string, List<string>>()
+        {
+            { nameof(RMB_Down), new List<string>() },
+            { nameof(LMB_Down), new List<string>() }
+        };
+
+        public List<string> GetMouseDownItems(int button)
+        {
+            switch (button)
+            {
+                case 0: return MouseDownEventOrder[nameof(LMB_Down)];
+                case 1: return MouseDownEventOrder[nameof(RMB_Down)];
+                default: return null;
+            }
+        }
+
+        public void OnMouseDown(MouseEventArgs e, string source_element)
+        {
+            var states = this.CheckButtonState(e);
+
+            if (LMB_Down)
+            {
+                MouseDownEventOrder[nameof(LMB_Down)].Add(source_element);
+                Console.WriteLine("Adding LMB");
+            }
+
+            if (RMB_Down)
+            {
+                MouseDownEventOrder[nameof(RMB_Down)].Add(source_element);
+                Console.WriteLine("Adding RMB");
+            }
+        }
+
+        public void OnMouseUp(MouseEventArgs e)
+        {
+            var states = this.CheckButtonState(e);
+            if (!LMB_Down)
+            {
+                MouseDownEventOrder[nameof(LMB_Down)].ForEach((a) => { RegisteredElements[a].OnMouseUp(); });
+                MouseDownEventOrder[nameof(LMB_Down)].Clear();
+            }
+            if (!RMB_Down)
+            {
+                MouseDownEventOrder[nameof(RMB_Down)].ForEach((a) => { RegisteredElements[a].OnMouseUp(); });
+                MouseDownEventOrder[nameof(RMB_Down)].Clear();
+            }
+        }
+
+        public void OnMouseOver(MouseEventArgs e)
+        {
+            this.ScreenX = (int)e.ScreenX;
+            this.ScreenY = (int)e.ScreenY;
+            this.ScreenX = (int)e.ClientX;
+            this.ScreenY = (int)e.ClientY;
+        }
+
+        public void OnMouseMove(MouseEventArgs e)
+        {
+            this.ScreenX = (int)e.ScreenX;
+            this.ScreenY = (int)e.ScreenY;
+            this.ScreenX = (int)e.ClientX;
+            this.ScreenY = (int)e.ClientY;
+
+            if (LMB_Down)
+            {
+                //this.MouseDownEventOrder[nameof(LMB_Down)].Where(a=>a.)
+            }
+
+        }
+
+        public void OnClick(MouseEventArgs e)
+        {
+            this.ScreenX = (int)e.ScreenX;
+            this.ScreenY = (int)e.ScreenY;
+            this.ScreenX = (int)e.ClientX;
+            this.ScreenY = (int)e.ClientY;
+        }
+
+    }
+}
